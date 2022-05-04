b40e68e31894107a3d55d4246e71085efb9835b1
Asperun <carnageepl@gmail.com>
2022-05-04 17:20:35 +0200

    exception: added exception handling user wanting to register at 2 conferences in same time frame.

---
+++ src/main/java/pl/fantasea/scheduler/exception/ConferenceUserNotEligibleException.java
@@ -0,0 +1,7 @@
+package pl.fantasea.scheduler.exception;
+
+public class ConferenceUserNotEligibleException extends RuntimeException {
+    public ConferenceUserNotEligibleException(String message) {
+        super(message);
+    }
+}
